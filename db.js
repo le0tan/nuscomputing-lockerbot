3f792b54667b814b914e27abf96fe7ceb4c6810b
Tan Yuanhong <leotan.yh@gmail.com>
2018-10-11 20:26:40 +0800

    [Feature] User registration

--- db.js
+++ db.js
@@ -9,29 +9,43 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: DB_HOST,
   user: DB_USER,
-  password: DB_PASSWORD
+  password: DB_PASSWORD,
+  database: "lockerbot_db"
 });
+
+module.exports.con = con;
+
+var cmd = "INSERT INTO users (chat_id, ivle_token) VALUES (123,'asd');";
 
 con.connect(function (err) {
   if (err) throw err;
-  console.log("Connected!");
+  console.log("DB Connected!");
 });
 
-/**
- * 
- *
- * @param {*} command
- */
 function queryWithLog(command) {
+  console.log('----------------queryWithLog Start----------------')
   console.log("Command: " + command);
   con.query(command, function (err, result) {
     if (err) throw err;
     console.log("Result: " + result);
+    console.log('----------------queryWithLog End----------------');
   });
 }
-
 
 module.exports.initialize = function () {
   queryWithLog("USE lockerbot_db");
   // queryWithLog("")
 }
+
+module.exports.addNewUser = function (chatId, ivleToken) {
+  var cmd = 'insert into users(chat_id, ivle_token) values (' + chatId + ', "' + ivleToken + '");';
+  queryWithLog(cmd);
+};
+
+// module.exports.isRegistered = function(chatId){
+//   var cmd = 'select count (*) from users where chat_id = 450448905;';
+//   con.query(cmd, function(err, res){
+//     if(err) throw err;
+//     else 
+//   })
+// };
--- index.js
+++ index.js
@@ -1,33 +1,83 @@
+//import modules
 const TelegramBot = require('node-telegram-bot-api');
-// const database = require('./db');
+const database = require('./db');
+const axios = require('axios');
 require("dotenv").config();
-const {BOT_TOKEN, IVLE_API_KEY, IVLE_URL_CALLBACK} = process.env;
 
+//create global constants
+const {BOT_TOKEN, IVLE_API_KEY, IVLE_URL_CALLBACK} = process.env;
 const bot = new TelegramBot(BOT_TOKEN, {polling: true});
 
+//main logic
 bot.on('message', (msg) => {
+  //extract command keyword and argument
   const command = msg.text.split(' ')[0];
   const args = msg.text.substr(command.length + 1);
 
-  console.log(msg.chat.id);
+  // console.log(msg.chat.id);
 
   switch(command){
     case '/start':{
-      bot.sendMessage(
-        msg.chat.id, `Hi ${msg.chat.first_name}! Let's get you set up!`,
-        {
-          parse_mode: 'Markdown',
-          reply_markup: {
-            inline_keyboard: [
-              [{ text: 'Get IVLE Token', url: `https://ivle.nus.edu.sg/api/login/?apikey=${IVLE_API_KEY}&url=${IVLE_URL_CALLBACK}` }],
-            ],
-          },
-        },
-      );
+      //prevent the same user from multiple registrations
+      database.con.query('select count (*) from users where chat_id = ' + msg.chat.id + ';',
+        function(err, res){
+          if(err) throw err;
+          else {
+            if(JSON.parse(JSON.stringify(res[0]))['count (*)'] != 0){
+              bot.sendMessage(msg.chat.id, 'Sorry but you have registered using this Telegram account before.');
+            } else {
+              //otherwise guide the user through the registration
+              bot.sendMessage(
+                msg.chat.id, `Hi ${msg.chat.first_name}! Let's get you set up!`,
+                {
+                  parse_mode: 'Markdown',
+                  reply_markup: {
+                    inline_keyboard: [
+                      [{ text: 'Get IVLE Token', url: `https://ivle.nus.edu.sg/api/login/?apikey=${IVLE_API_KEY}&url=${IVLE_URL_CALLBACK}` }],
+                    ],
+                  },
+                },
+              );
+            }
+          }
+      });
       break;
     }
     case '/token':{
-      bot.sendMessage(msg.chat.id, "Your token is: "+args);
+      var token = args;
+      axios.get('https://ivle.nus.edu.sg/api/Lapi.svc/'+ 'UserID_Get' + '?APIKey='+ IVLE_API_KEY + '&Token=' + token)
+        .then(resp => {
+          var ivle_id = resp.data;
+          database.con.query('select count (*) from users where ivle_id = "' + ivle_id + '";',
+          function(err, res){
+            if(err) throw err;
+            else {
+              if(JSON.parse(JSON.stringify(res[0]))['count (*)'] != 0){
+                bot.sendMessage(msg.chat.id, 'Sorry but your IVLE ID has been registered before.');
+              } else {
+                axios.get('https://ivle.nus.edu.sg/api/Lapi.svc/'+ 'UserName_Get' + '?APIKey='+ IVLE_API_KEY + '&Token=' + token)
+                  .then(resp => {
+                    var ivle_name = resp.data;
+                    database.con.query(`insert into users(chat_id, ivle_token, ivle_id, ivle_name) values (${msg.chat.id}, "${token}", "${ivle_id}", "${ivle_name}");`,
+                    function(err, res){
+                      if(err) throw err;
+                      else {
+                        console.log(`Successfully added user ${ivle_id}!`)
+                      }
+                    });
+                  })
+                  .catch(err => {
+                    console.log(err);
+                  });
+
+              }
+            }
+          });          
+        })
+        .catch(err => {
+          console.log(err);
+        });
+
       break;
     }
     default:{
@@ -36,3 +86,8 @@ bot.on('message', (msg) => {
     }
   }
 });
+
+// https://ivle.nus.edu.sg/api/Lapi.svc/UserID_Get?APIKey={System.String}&Token={System.String}
+
+//use this to access ivle api
+//axios.get('https://ivle.nus.edu.sg/api/Lapi.svc/'+ api + '?APIKey='+ IVLE_API_KEY + '&Token=' + token)
