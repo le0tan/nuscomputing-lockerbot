f5c2ecb33d9a59decb0dc5cad00144417c347a74
Tan Yuanhong <leotan.yh@gmail.com>
2018-10-13 11:40:50 +0800

    [Deploy] restarter

--- restarter.py
+++ restarter.py
@@ -1,11 +1,20 @@
 import os
 import time
 
-process_name = "node index.js"
+
+process_name_1 = "node index.js"
+process_name_2 = "sudo node token_server.js"
+
+# def sudo_command(command):
+#     print('echo %s|sudo -S %s' % (, command))
+
 while True:
     tmp = os.popen("ps -Af").read()
-    if process_name not in tmp:
-        now = time.asctime(time.localtime())
-        os.system(process_name)
-        print(process_name + " is restarted at " + now)
-        time.sleep(10)
+    now = time.asctime(time.localtime())
+    if process_name_1 not in tmp:
+        os.system(process_name_1)
+        print(process_name_1 + " is restarted at " + now)
+    if process_name_2 not in tmp:
+        sudo_command(process_name_2)
+        print(process_name_2 + " is restarted at " + now)
+    time.sleep(1)
